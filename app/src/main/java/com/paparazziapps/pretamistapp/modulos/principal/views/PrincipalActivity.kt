84072c0c43dc634d24d43cb298df038dcfe8b32b
Alikhan <73962071@continental.edu.pe>
2022-05-08 22:32:53 -0500

    responsive all screens

--- app/src/main/java/com/paparazziapps/pretamistapp/modulos/principal/views/PrincipalActivity.kt
+++ app/src/main/java/com/paparazziapps/pretamistapp/modulos/principal/views/PrincipalActivity.kt
@@ -45,17 +45,15 @@ class PrincipalActivity : AppCompatActivity() {
         bottomNavigationView = binding.navView
         toolbar              = binding.tool.toolbar
 
+        /////////////////////////////////////////FREE TRIAL
+        /*
         var fecha7Dias:Long = 1652651285000  // fechaPasado -> 1647147600000 o fechaSuperior -->1649826000000
 
         if(getFechaActualNormalInUnixtime().minus(fecha7Dias) > 0)
         {
             println("Fecha actual normal: ${getFechaActualNormalInUnixtime().minus(fecha7Dias)}")
             binding.cortinaFreeTrial.isVisible = true
-        }
-
-
-
-
+        }*/
 
         setUpBottomNav()
         setUpInicialToolbar()
